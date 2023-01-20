604ba511db3562be854fac9b984e6845f5b86ac6
poojakrp777 <poojakrp777@gmail.com>
2023-01-20 22:55:22 +0530

    [Pooja]Employee Part time Wage calculating

--- Program.cs
+++ Program.cs
@@ -12,24 +12,34 @@ namespace EmployeeWageCompute
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
             int Is_Present = 1;
+            int Part_Time = 2;
             const int EmpWagePerHr = 20;
             int empHrs = 0, empWage = 0;
 
             //UC-1 Check employee present or not
             Random random = new Random();
-            int EmpCheck = random.Next(0, 2);
+            int EmpCheck = random.Next(0, 3); //the range will take 0,1,2
+
             if (EmpCheck == Is_Present)
             {
                 Console.WriteLine("Employee is present");
                 empHrs = 8;
             }
+
+            //UC3 Calculating Emp Part time wage
+            else if (EmpCheck == Part_Time)
+            {
+                Console.WriteLine("Employee is present");
+                empHrs = 4;
+            }
             else
             {
                 Console.WriteLine("Employee is absent");
+                // empHrs = 0; if emp is absent it's automatically take 0
 
             }
 
-            //UC-2 calculate EmployeeWage
+            //UC-2 calculate Employee daily Wage
             empWage = EmpWagePerHr * empHrs;
             Console.WriteLine("Employee Wage is:" + empWage);
             Console.ReadLine();
