1cf0d01685bd5350f49f65cd1a7e5d3458404e01
poojakrp777 <poojakrp777@gmail.com>
2023-01-20 22:42:45 +0530

    [Pooja] Calculating Employee Daily Wage

--- Program.cs
+++ Program.cs
@@ -12,16 +12,27 @@ namespace EmployeeWageCompute
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
             int Is_Present = 1;
+            const int EmpWagePerHr = 20;
+            int empHrs = 0, empWage = 0;
+
+            //UC-1 Check employee present or not
             Random random = new Random();
             int EmpCheck = random.Next(0, 2);
             if (EmpCheck == Is_Present)
             {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+
             }
+
+            //UC-2 calculate EmployeeWage
+            empWage = EmpWagePerHr * empHrs;
+            Console.WriteLine("Employee Wage is:" + empWage);
+            Console.ReadLine();
         }
     }
 }
